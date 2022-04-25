85d3e2f9e3ceb7938ba113a1380fb19dd2086904
narekkarimyan <narekkarimyan@gmail.com>
2022-04-25 12:47:24 +0400

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,7 +1,7 @@
 const secrets = {
   dbUri: 'mongodb+srv://dev:zLX06YwhGruvrN8h@anvtangertevekutyun.ff5gu.mongodb.net/db?retryWrites=true&w=majority',
-  mail: 'anvtangertevekutyunmailer@gmail.com',
-  mailPass: 'AE515511',
+  mail: 'armenrub1991@mail.ru',
+  mailPass: 'armkis088300',
 };
 
 const getSecret = (key) => secrets[key];
